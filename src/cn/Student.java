9f71eebb93d51fc7500f257e993f3ecf437184d0
liuweidong <1198423691@qq.com>
2019-08-31 07:43:39 +0800

    the tirst commit

--- src/cn/Student.java
+++ src/cn/Student.java
@@ -3,6 +3,7 @@ package cn;
 public class Student {
     public static void main(String[] args) {
         System.out.println("this is my home");
+        System.out.println("list diid ");
     }
 
 }
